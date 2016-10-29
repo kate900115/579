29bdcb035eb1d2cc7b6f9e4f6b1c9b59f6ab6389
kate900115 <kate900115@gmail.com>
2016-10-29 15:27:13 -0400

    modified:   gate.h 	modified:   main.cpp

--- cpp/gate.h
+++ cpp/gate.h
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+enum GType{NOT, AND2, AND3, AND4, NAND2, NAND3, NAND4, OR2, OR3, OR4, NOR2, NOR3, NOR4, DFF};
+
 class Gate
 {
 	private:
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
+#include "gate.h"
+#include <string>
 
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
+	//read inputfile
+	string FileName = string(argv[1]);
+	ifstream inFile;
+	inFile.open(FileName);
+	
+	if (!inFile.good())
+	{
+		cout << "ERROR in read_input() - cannot open " << FileName << " for reading." << endl;
+	}
+	
+	//store the inputfile into the circuit data structure
 
 }
 
