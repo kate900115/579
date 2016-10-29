bc6efa9f60d1726a639b54670da7a8071a5a0a1b
kate900115 <kate900115@gmail.com>
2016-10-29 14:37:02 -0400

    new file:   cpp/DValue.h 	new file:   cpp/gate.cpp 	new file:   cpp/gate.h 	new file:   cpp/main.cpp 	new file:   cpp/wire.cpp 	new file:   cpp/wire.h

---
+++ cpp/wire.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "DValue.h"
+
+using namespace std;
+
+class Wire
+{
+	private:
+		string WName;
+		string WType;
+		bool IsStack;
+		DValue value;
+
+	public:
+		//constructor
+		Wire (string name, string type, bool is_stack, DValue v){WName = name; WType = type; IsStack = is_stack; value = v;}
+
+		
+}
